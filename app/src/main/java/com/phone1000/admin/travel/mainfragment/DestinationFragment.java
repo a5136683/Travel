f159ad4e9976f1e26cfdc224168ef925c59eeeb5
liang <540899652@qq.com>
2016-11-18 11:19:37 +0800

    Initial commit

---
+++ app/src/main/java/com/phone1000/admin/travel/mainfragment/DestinationFragment.java
@@ -0,0 +1,24 @@
+package com.phone1000.admin.travel.mainfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.phone1000.admin.travel.R;
+
+
+/**
+ * Created by Administrator on 2016/11/17.
+ */
+
+public class DestinationFragment extends android.support.v4.app.Fragment {
+    private View v=null;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v=LayoutInflater.from(getActivity()).inflate(R.layout.destination_fragment_layout,null);
+        return v;
+    }
+}
